93f3fab22477d16610c64b655659e24c0ca27f2b
Azhar <azhar.subair.2@gmail.com>
2026-02-27 18:03:24 +0530

    first commit-0

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [file, setFile] = useState(null);
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleUpload = async () => {
+    if (!file) return alert("Select an image");
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    setLoading(true);
+
+    try {
+      const res = await axios.post(
+        "http://localhost:9001/predict",
+        formData
+      );
+
+      setResult(res.data);
+    } catch (err) {
+      alert("Error uploading image");
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>🌿Paddyscan</h1>
+
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+
+      <br /><br />
+
+      <button onClick={handleUpload}>
+        Detect Disease
+      </button>
+
+      {loading && <p>Analyzing image...</p>}
+
+      {result && (
+        <div style={{ marginTop: 20 }}>
+          <h2>Result</h2>
+          <p><b>Disease:</b> {result.disease}</p>
+          <p><b>Confidence:</b> {result.confidence}</p>
+          <p><b>Treatment:</b> {result.treatment}</p>
+          <p><b>Prevention:</b> {result.prevention}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
